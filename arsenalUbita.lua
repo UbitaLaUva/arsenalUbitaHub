fd490ff2a24d3a769a887022a7351b7196dc4143
UbitaLaUva <96164113+UbitaLaUva@users.noreply.github.com>
2023-01-26 01:37:19 -0500

    Update arsenalUbita.lua

--- arsenalUbita.lua
+++ arsenalUbita.lua
@@ -14,11 +14,11 @@ local utility = {}
 -- themes
 local objects = {}
 local themes = {
-	Background = Color3.fromRGB(24, 24, 24), 
+	Background = Color3.fromRGB(109,48,141), 
 	Glow = Color3.fromRGB(0, 0, 0), 
-	Accent = Color3.fromRGB(10, 10, 10), 
+	Accent = Color3.fromRGB(93,14,144), 
 	LightContrast = Color3.fromRGB(20, 20, 20), 
-	DarkContrast = Color3.fromRGB(14, 14, 14),  
+	DarkContrast = Color3.fromRGB(46, 16, 75),  
 	TextColor = Color3.fromRGB(255, 255, 255)
 }
 
