d3a793f4244e451e7abce73b2c28d6b7cd6e9bf6
UbitaLaUva <96164113+UbitaLaUva@users.noreply.github.com>
2023-01-29 22:38:24 -0500

    Update keysystem.lua

--- keysystem.lua
+++ keysystem.lua
@@ -9,11 +9,11 @@ function get_random_key()
   for key, value in pairs(keys) do
     keys_list[#keys_list + 1] = key
   end
-  return keys_list[math.random(#keys_list)]
+  return keys_list[math.floor(math.random() * #keys_list) + 1]
 end
 
 add_key("Key1", "Value1")
 add_key("Key2", "Value2")
 add_key("Key3", "Value3")
 
-return keys
+print(get_random_key()) -- imprime una clave al azar, por ejemplo "Key2"
