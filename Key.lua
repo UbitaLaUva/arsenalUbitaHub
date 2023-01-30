7dbda3790ce644cbe73713c01231c6b1bdcb614b
UbitaLaUva <96164113+UbitaLaUva@users.noreply.github.com>
2023-01-29 23:57:11 -0500

    Update Key.lua

--- Key.lua
+++ Key.lua
@@ -1,8 +1,2 @@
-local keys = {"ubita", "otra_clave", "otra_clave_mas"}
-
-function get_random_key()
-  return keys[math.random(#keys)]
-end
-
-local random_key = get_random_key()
-return random_key
+local keys = helloworld
+return keys
