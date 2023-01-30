0cdae8d076fc9a055359dcf185446617596483ff
UbitaLaUva <96164113+UbitaLaUva@users.noreply.github.com>
2023-01-29 22:36:29 -0500

    Update keysystem.lua

--- keysystem.lua
+++ keysystem.lua
@@ -1,2 +1,17 @@
-local key = "ubita"
-return key
+local keys = {}
+
+function add_key(key, value)
+  keys[key] = value
+end
+
+function get_random_key()
+  local keys_list = {}
+  for key, value in pairs(keys) do
+    keys_list[#keys_list + 1] = key
+  end
+  return keys_list[math.random(#keys_list)]
+end
+
+add_key("Key1", "Value1")
+add_key("Key2", "Value2")
+add_key("Key3", "Value3")
