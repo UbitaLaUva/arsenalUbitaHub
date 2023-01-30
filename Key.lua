3cfb3a536f097ba889ad769c138ea001831e3a6e
UbitaLaUva <96164113+UbitaLaUva@users.noreply.github.com>
2023-01-29 22:47:36 -0500

    Update Key.lua

--- Key.lua
+++ Key.lua
@@ -21,6 +21,6 @@ add_key("Key2", "Value2")
 add_key("Key3", "Value3")
 
 local random_key = get_random_key()
-return random_key
+
 print(random_key) -- imprime una clave al azar, por ejemplo "Key2"
 print(get_value(random_key)) -- imprime el valor asociado con la clave al azar, por ejemplo "Value2"
